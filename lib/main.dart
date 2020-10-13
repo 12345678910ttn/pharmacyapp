8d46cc6bc7958b1fcdb55d5d247ccc3448fe4e4c
TinTinNwe <tintinnwee@gmail.com>
2020-10-13 13:29:13 +0630

    Try First

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,7 +10,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Public Application For Emergency Help',
+      debugShowCheckedModeBanner:false,
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -26,102 +28,141 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home:SplashLogo(),
+    );
+  }
+}
+class SplashLogo  extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new  SplashScreen (
+      seconds: 14,
+      navigateAfterSeconds: MainDashboard(),
+      title: new Text('Welcome To Public Emergency',
+        style: new TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 20.0
+        ),
+      ),
+       image: new Image.network('https://flutter.io/images/catalog-widget-placeholder.png'),
+      // backgroundGradient: new LinearGradient(colors: [Colors.cyan, Colors.blue], begin: Alignment.topLeft, end: Alignment.bottomRight),
+      backgroundColor: Colors.white,
+      styleTextUnderTheLoader: new TextStyle(),
+      photoSize: 100.0,
+      onClick: ()=>print("Hello Public"),
+      loaderColor: Colors.red,
+      
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class MainDashboard extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MainDashboardState createState() => _MainDashboardState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _MainDashboardState extends State<MainDashboard> {
+  final list_item =[
+    {
+      "name":"Image 1",
+      "pic": "pharmacy2.jpg",
+      "price":"70"
+    },
+    {
+      "name":"Image 2",
+      "pic": "hospital.jpg",
+      "price":"80"
+    },
+    {
+      "name":"Image 3",
+      "pic": "volunteer.png",
+      "price":"60"
+    },
+    {
+      "name":"Image 4",
+      "pic": "covid.jpg",
+      "price":"60"
+    },
+ 
+  ];
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      appBar: new AppBar(
+        title:Text("DASH BOARD (Mandalay Directory)"),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'MY first App In Repository',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: Container(
+        // color: Colors.blueAccent,
+        child: GridView.builder(
+          itemCount: list_item.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+          itemBuilder: (BuildContext context,int index){
+            return Product(
+              list_item[index]['name'],
+               list_item[index]['pic'],
+                list_item[index]['price']
+
+            );
+          },
+          
         ),
+        decoration: BoxDecoration(
+    color: Colors.white,
+    borderRadius: BorderRadius.only(
+      topLeft: Radius.circular(10),
+        topRight: Radius.circular(10),
+        bottomLeft: Radius.circular(10),
+        bottomRight: Radius.circular(10)
+    ),
+    boxShadow: [
+      BoxShadow(
+        color: Colors.blue.withOpacity(0.5),
+        spreadRadius: 5,
+        blurRadius: 7,
+        offset: Offset(0, 3), // changes position of shadow
       ),
-      floatingActionButton: Row(
-        children: [
-          FloatingActionButton(
-            onPressed: _incrementCounter,
-            tooltip: 'Increment ',
-            child: Icon(Icons.add),
-          ),
-          FloatingActionButton(
-            onPressed: _incrementCounter,
-            tooltip: 'Decrement',
-            child: Icon(Icons.remove),
-          ),
-        ],
-      ), 
-// This trailing comma makes auto-formatting nicer for build methods.
+    ],
+    )
+      ),
+    );
+  }
+}
+class Product extends StatelessWidget {
+  final product_name;
+  final product_pic;
+  final product_price;
+  Product(this.product_name,this.product_pic,this.product_price);
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Container(
+        child: Card(
+          child: Hero(tag: product_name, 
+          child:InkWell(
+            onTap:(){},
+            child:GridTile(child: Image.asset("assets/"+ product_pic),)
+          )),
+          
+        ),
+         decoration: BoxDecoration(
+    color: Colors.white,
+    borderRadius: BorderRadius.only(
+      topLeft: Radius.circular(50),
+        topRight: Radius.circular(50),
+        bottomLeft: Radius.circular(50),
+        bottomRight: Radius.circular(50)
+    ),
+    boxShadow: [
+      BoxShadow(
+        color: Colors.blue.withOpacity(0.5),
+        spreadRadius: 5,
+        blurRadius: 7,
+        offset: Offset(0, 3), // changes position of shadow
+      ),
+    ],
+    )
+      ),
     );
   }
 }
